bce99efed9e3f80f05ecaed9ff22848d32063e03
zombieyang(杨浩) <zombieyang@tencent.com>
2022-08-12 20:58:59 +0800

    [unity] support generation for generic types with constraints && fix the constraints filter ignored optional parameters fix #937

--- unity/general/Src/UnitTest/GenTest.cs
+++ unity/general/Src/UnitTest/GenTest.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace Puerts.UnitTest 
 {
     class Singleton1<T> where T: Singleton1<T>, new() {}
     class Zombie: Singleton1<Zombie> {}
-    class Singleton2<T> where T: class, new() {}
 
     [TestFixture]
     public class GenUnitTest
@@ -21,7 +21,7 @@ namespace Puerts.UnitTest
             Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<int, JsEnv>), genList);
 
             string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T>"));
+            Assert.True((new Regex(@"<TValue>")).IsMatch(wrapperContent));
         }
 
         [Test]
@@ -36,7 +36,7 @@ namespace Puerts.UnitTest
             Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<Type, JsEnv>), genList);
 
             string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T,S>"));
+            Assert.True((new Regex(@"<TKey,TValue>")).IsMatch(wrapperContent));
         }
 
         [Test]
@@ -51,7 +51,8 @@ namespace Puerts.UnitTest
             Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton1<Zombie>), genList);
 
             string wrapperContent = wrapRender(wrapperInfo);
-            Assert.False(wrapperContent.Contains("<T>"));
+            System.Console.WriteLine(wrapperContent);
+            Assert.True((new Regex(@"where T : Puerts.UnitTest.Singleton1<T>")).IsMatch(wrapperContent));
         }
 
         [Test]
@@ -60,13 +61,82 @@ namespace Puerts.UnitTest
             var jsEnv = new JsEnv(new TxtLoader());
             var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
             var genList = new List<Type>() {
-                typeof(Singleton2<Zombie>),            
-                typeof(Zombie)             
+                typeof(List<int>)            
             };
-            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton2<Zombie>), genList);
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
 
             string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T>"));
+            Assert.False((new Regex(@"<\w>")).IsMatch(wrapperContent));
+        }
+
+        [Test]
+        public void GenericWrapperWithConstraint_1()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Puerts.UnitTest.GenericWrapperWithConstraintStruct<TimeSpan>)         
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True((new Regex(@"where T : struct")).IsMatch(wrapperContent));
+        }
+
+        [Test]
+        public void GenericWrapperWithConstraint_2()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Puerts.UnitTest.GenericWrapperWithConstraintClass<JsEnv>)         
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True((new Regex(@"where T : class")).IsMatch(wrapperContent));
+        }
+
+        [Test]
+        public void GenericWrapperWithConstraint_3()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Puerts.UnitTest.GenericWrapperWithConstraintNew<JsEnv>)         
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True((new Regex(@"where T : new\(\)")).IsMatch(wrapperContent));
+        }
+
+        [Test]
+        public void GenericWrapperWithConstraint_4()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Puerts.UnitTest.GenericWrapperWithConstraintNewClass<JsEnv>)         
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True((new Regex(@"where T : class, new\(\)")).IsMatch(wrapperContent));
+        }
+
+        [Test]
+        public void GenericWrapperWithConstraint_5()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Puerts.UnitTest.GenericWrapperWithConstraintStringComparer<StringComparer>)         
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(genList[0], genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True((new Regex(@"where T : System.StringComparer")).IsMatch(wrapperContent));
         }
     }
 }
